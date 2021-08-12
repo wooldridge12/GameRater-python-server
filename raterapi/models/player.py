86d5c62a065c928010adb1015a6531a53848b321
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-12 16:23:10 -0400

    Able to register and login

--- raterapi/models/player.py
+++ raterapi/models/player.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Player(models.Model):
-    user_name = models.CharField(max_length=100)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=50)
     
