a910e20ba2f3e38fc4ef220cf3a5528f66c5dcea
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-13 15:13:41 -0400

    games show on dom

--- rater/urls.py
+++ rater/urls.py
@@ -1,28 +1,31 @@
-"""rater URL Configuration
+# from django.contrib import admin
+# from django.urls import path
+# from django.conf.urls import include
+# from raterapi.views import register_user, login_user
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# urlpatterns = [
+#     # Requests to http://localhost:8000/register will be routed to the register_user function
+#     path('register', register_user),
+#     # Requests to http://localhost:8000/login will be routed to the login_user function
+#     path('login', login_user),
+#     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+#     path('admin/', admin.site.urls),
+# ]
+
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from rest_framework import routers
 from raterapi.views import register_user, login_user
+from raterapi.views import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
-    # Requests to http://localhost:8000/register will be routed to the register_user function
+    path('', include(router.urls)),
     path('register', register_user),
-    # Requests to http://localhost:8000/login will be routed to the login_user function
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
