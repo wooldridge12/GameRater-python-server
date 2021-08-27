b61c80850299e38fea9d488bd4096056906f2030
wooldridge12 <wooldridgemikey@gmail.com>
2021-08-27 15:25:22 -0400

    state is set to save categories

---
+++ raterapi/views/category.py
@@ -0,0 +1,33 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from raterapi.models import Category
+
+
+class CategoryView(ViewSet):
+
+    def retrieve(self, request, pk=None):
+        try:
+            category = Category.objects.get(pk=pk)
+            serializer = CategorySerializer(category, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        categories = Category.objects.all()
+
+        # Note the addtional `many=True` argument to the
+        # serializer. It's needed when you are serializing
+        # a list of objects instead of a single object.
+        serializer = CategorySerializer(
+            categories, many=True, context={'request': request})
+        return Response(serializer.data)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
